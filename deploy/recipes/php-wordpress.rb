3a6c4bf03a24ed9915e59fe63b013750b5f197d1
Drew J. Sonne <drew.sonne@gmail.com>
2013-09-01 10:10:20 +1000

    Added wordpress recipe.

---
+++ deploy/recipes/php-wordpress.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: deploy
+# Recipe:: php
+#
+
+include_recipe 'deploy'
+
+
+
+node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'php'
+    Chef::Log.debug("Skipping deploy::wordpress application #{application} as it is not an PHP app")
+    next
+  end
+
+  wordpress_deploy_dir do
+    user deploy[:user]
+    path deploy[:deploy_to]  + "/current/wp-content"
+  end
+
+end
+
